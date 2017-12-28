5a50e5ca797e80c421576c66c94b288f99ba2d4f
jimm977 <33897796+jimm977@users.noreply.github.com>
2017-12-29 02:42:41 +0300

    1
    2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## First function creates a template for object that caches a matrix and it's inverse
+## and 'methods' to deal with them (read and write them)
+## Second function checks whether we need to calculate new inverse or not (which is illogical
+## to me, at least yet, I've created a post at the forum on the matter)
 
-## Write a short comment describing this function
+## Cache for matrix, it's inverse, plus four methods (read/write matrix/it's inverse)
+## There's also a flag indicating whether a new matrix was set (so we need to re-calculate 
+## the inverse)
 
-makeCacheMatrix <- function(x = matrix()) {
-
+MakeCacheMatrix<-function(CurMatrix=matrix()) {
+  FlagInvMatrixCalculated<-FALSE
+  InvMatrix=matrix()
+  set_matrix<-function(NewMtx)
+  {
+    CurMatrix<<-NewMtx
+    FlagInvMatrixCalculated<<-FALSE
+  }
+  set_inverse<-function(InvCalc)
+  {
+    InvMatrix<<-InvCalc
+    FlagInvMatrixCalculated<<-TRUE
+  }
+  get_cur_matrix<-function() CurMatrix
+  get_inverse<-function() InvMatrix
+  
+  list(set_matrix=set_matrix, set_inverse=set_inverse,
+       get_cur_matrix=get_cur_matrix, get_inverse=get_inverse, flag=FlagInvMatrixCalculated)
 }
 
+## Here we instantiate a new 'MakeCacheMatrix' object, named MatrixList
 
-## Write a short comment describing this function
+MatrixList<-MakeCacheMatrix(matrix(1:4,2,2))
 
-cacheSolve <- function(x, ...) {
+
+## We check whether it's a new matrix or not, and if it's new, we calculate (and write to cache)
+## new inverse, otherwise we read inverse from cache
+
+cacheSolve <- function(MatrixList) {
         ## Return a matrix that is the inverse of 'x'
+  if(!MatrixList$flag) ## we need to calculate the inverse
+  { 
+    NewInverse<-solve(MatrixList$get_cur_matrix())
+    MatrixList$set_inverse(NewInverse)
+  }  else  {
+    message("Getting cached data")
+    NewInverse<-MatrixList$get_inverse()
+  }
+  NewInverse
 }
